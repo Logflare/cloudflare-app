02a2883c095c3a9cc0d63c6162f7d6b72f2971dc
chasers <chase@pagemodified.com>
2019-01-10 09:50:28 -0700

    More yarn fixes

--- workers/lfworker.js
+++ workers/lfworker.js
@@ -3,25 +3,25 @@ addEventListener("fetch", event => {
 })
 
 async function handleRequest(request) {
-  rMeth = request.method
-  rUrl = request.url
-  uAgent = request.headers.get("user-agent")
-  cfRay = request.headers.get("cf-ray")
+  var rMeth = request.method
+  var rUrl = request.url
+  var uAgent = request.headers.get("user-agent")
+  var cfRay = request.headers.get("cf-ray")
 
-  sourceKey = "12a26fa2-b9ea-4d4b-9209-dc9fc25fb758"
-  apiKey = "SL7NBVZbxN1C"
+  var sourceKey = "12a26fa2-b9ea-4d4b-9209-dc9fc25fb758"
+  var apiKey = "SL7NBVZbxN1C"
 
   const response = await fetch(request)
 
-  statusCode = response.status
-  contentLength = response.headers.get("content-legth")
+  var statusCode = response.status
+  var contentLength = response.headers.get("content-legth")
 
-  log_entry = `${rMeth} | ${statusCode} | ${cfRay} | ${rUrl} | ${uAgent}`
+  var logEntry = `${rMeth} | ${statusCode} | ${cfRay} | ${rUrl} | ${uAgent}`
 
   const init = {
     method: "POST",
     headers: { "X-API-KEY": api_key, "Content-Type": "application/json" },
-    body: JSON.stringify({ source: source_key, log_entry }),
+    body: JSON.stringify({ source: source_key, logEntry }),
   }
 
   const logflare = await fetch("https://logflare.app/api/logs", init)
