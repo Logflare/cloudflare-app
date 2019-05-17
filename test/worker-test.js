a8f1a8810ede2a88173dc3775e6c0a38ac01a7b0
ontofractal <ontofractal@protonmail.com>
2019-05-17 16:41:53 +0300

    add test for fetchIpData worker function

--- test/worker-test.js
+++ test/worker-test.js
@@ -1,4 +1,5 @@
 /* eslint-disable global-require */
+/* eslint-disable no-undef */
 
 before(async () => {
   Object.assign(
@@ -8,11 +9,21 @@ before(async () => {
     ).context,
   )
 })
-
 const assert = require("assert")
 
 describe("Cloudflare Worker test", () => {
-  it("handlesRequest", async () => {
-    assert.equal(true, true)
+  it("correctly fetches IP data from ipinfo.io", async () => {
+    const ipData = await fetchIpData("8.8.8.8")
+    assert.deepEqual(ipData, {
+      city: "Mountain View",
+      country: "US",
+      hostname: "google-public-dns-a.google.com",
+      ip: "8.8.8.8",
+      loc: "37.3860,-122.0840",
+      org: "AS15169 Google LLC",
+      phone: "650",
+      postal: "94035",
+      region: "California",
+    })
   })
 })
